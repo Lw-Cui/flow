52f8ff907e5e3df7dba98f46b545711ba1200e72
liwei <cuigragon@gmail.com>
2020-10-04 09:38:26 -0400

    change map name

--- src/parser/desugar.ml
+++ src/parser/desugar.ml
@@ -1,6 +1,6 @@
 #require "flow_parser";;
 
-module Map = Map.Make(String);;
+module SMap = Map.Make(String);;
 
 
 type expr = 
@@ -14,7 +14,7 @@ type expr =
 ;;
 
 
-let desugar_literal (ctx: bool Map.t) (l: Loc.t Flow_ast.Literal.t): expr =
+let desugar_literal (ctx: bool SMap.t) (l: Loc.t Flow_ast.Literal.t): expr =
     match l with {value = value} -> 
     match value with
     | Number n -> Num n
@@ -22,7 +22,7 @@ let desugar_literal (ctx: bool Map.t) (l: Loc.t Flow_ast.Literal.t): expr =
 ;;
 
 
-let desugar_expr (ctx: bool Map.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.t): expr =
+let desugar_expr (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.t): expr =
     let e' = snd e in 
     match e' with
     | Literal l -> desugar_literal ctx l
@@ -30,20 +30,20 @@ let desugar_expr (ctx: bool Map.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.t): ex
 ;;
 
 
-let desugar_declarator_init (ctx: bool Map.t) (init: (Loc.t, Loc.t) Flow_ast.Expression.t option): expr =
+let desugar_declarator_init (ctx: bool SMap.t) (init: (Loc.t, Loc.t) Flow_ast.Expression.t option): expr =
     match init with
     | None -> Undefined
     | Some init -> desugar_expr ctx init
 ;;
 
-let desugar_declarator (ctx: bool Map.t) (decl: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.Declarator.t): expr =
+let desugar_declarator (ctx: bool SMap.t) (decl: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.Declarator.t): expr =
     let decl' = snd decl in
     match decl' with {id = id; init = init} ->
     let id' = snd id in 
     match id' with
     | Identifier {name = name} -> (
         let name' = snd name in
-        match Map.find_opt name'.name ctx with
+        match SMap.find_opt name'.name ctx with
         | Some true -> raise @@ Failure "local variable is not supported" 
         | Some false -> raise @@ Failure "Not assignable"
         | None -> (* It's global. if it exists, do nothing, else set to undefined. *)
@@ -54,13 +54,13 @@ let desugar_declarator (ctx: bool Map.t) (decl: (Loc.t, Loc.t) Flow_ast.Statemen
 ;;
 
 
-let desugar_variableDeclaration (ctx: bool Map.t) (decls: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.t): expr =
+let desugar_variableDeclaration (ctx: bool SMap.t) (decls: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.t): expr =
     match decls with {declarations = declarations} ->
     List.fold_right (fun l r -> ESeq (l, r)) (List.map (desugar_declarator ctx) declarations) Undefined
 ;;
 
 (* statement is the top level element in js *)
-let desugar_stmt (ctx: bool Map.t) (stmt: (Loc.t, Loc.t) Flow_ast.Statement.t): expr =
+let desugar_stmt (ctx: bool SMap.t) (stmt: (Loc.t, Loc.t) Flow_ast.Statement.t): expr =
     let stmt' = snd stmt in
     match stmt' with
     | VariableDeclaration var ->  desugar_variableDeclaration ctx var
@@ -72,7 +72,7 @@ let desugar_stmt (ctx: bool Map.t) (stmt: (Loc.t, Loc.t) Flow_ast.Statement.t): 
 let desugar ((prog, _): (Loc.t, Loc.t) Flow_ast.Program.t * 'b): expr =
     let prog' = snd prog in 
     let stmts = prog'.statements in
-    List.fold_right (fun l r -> ESeq (l, r)) (List.map (desugar_stmt Map.empty) stmts) Undefined
+    List.fold_right (fun l r -> ESeq (l, r)) (List.map (desugar_stmt SMap.empty) stmts) Undefined
 ;;
 
-desugar @@ Parser_flow.program "var liwei = 0;"
+desugar @@ Parser_flow.program "var liwei = 0, ocaml = 6;"
