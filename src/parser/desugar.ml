4b4c73f20be07f984e3f10d41d1dff63cb7ff444
liwei <cuigragon@gmail.com>
2020-10-05 20:52:45 -0400

    support Object

--- src/parser/desugar.ml
+++ src/parser/desugar.ml
@@ -5,39 +5,80 @@ module SMap = Map.Make(String);;
 
 type expr = 
     | Id of string
-    | String of string
+    | EString of string
     | Undefined 
     | SetRef of expr * expr
     | Deref of expr
     | UpdateField of expr * expr * expr
     | ESeq of expr * expr
     | Num of float
+    | Object of (string * expr) list
 ;;
 
 
-let desugar_literal (ctx: bool SMap.t) (l: Loc.t Flow_ast.Literal.t): expr =
+let rec to_string (e: expr) : string = 
+    match e with
+    | EString s -> s
+    | _ -> raise @@ Failure "Unsupported conversion"
+
+and
+
+desugar_literal (ctx: bool SMap.t) (l: Loc.t Flow_ast.Literal.t): expr =
     match l with {value = value} -> 
     match value with
     | Number n -> Num n
+    | String n -> EString n
     | _ -> raise @@ Failure "Unsupported literal"
-;;
 
+and
 
-let desugar_expr (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.t): expr =
+desugar_properties_init_key (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.Object.Property.key): expr = 
+    match e with
+    | Literal l -> desugar_literal ctx @@ snd l
+    | _ -> raise @@ Failure "Unsupported literal"
+
+and
+
+desugar_property (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.Object.property): (string * expr) = 
+    match e with
+    | SpreadProperty p -> raise @@ Failure "SpreadProperty is not supported"
+    | Property p -> (
+        let p' = snd p in 
+        match p' with
+        | Init {key = key; value = value} -> (to_string @@ desugar_properties_init_key ctx key, desugar_expr ctx value)
+        | _ -> raise @@ Failure "Unsupported property type"
+    )
+
+and
+
+desugar_properties (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.Object.property list): expr = 
+    Object (List.map (desugar_property ctx) e)
+
+and
+
+desugar_object (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.Object.t): expr = 
+    match e with {properties = properties} ->
+        desugar_properties ctx properties
+
+and
+
+desugar_expr (ctx: bool SMap.t) (e: (Loc.t, Loc.t) Flow_ast.Expression.t): expr =
     let e' = snd e in 
     match e' with
     | Literal l -> desugar_literal ctx l
+    | Object obj -> desugar_object ctx obj
     | _ -> raise @@ Failure "Unsupported expression"
-;;
 
+and
 
-let desugar_declarator_init (ctx: bool SMap.t) (init: (Loc.t, Loc.t) Flow_ast.Expression.t option): expr =
+desugar_declarator_init (ctx: bool SMap.t) (init: (Loc.t, Loc.t) Flow_ast.Expression.t option): expr =
     match init with
     | None -> Undefined
     | Some init -> desugar_expr ctx init
-;;
 
-let desugar_declarator (ctx: bool SMap.t) (decl: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.Declarator.t): expr =
+and
+
+desugar_declarator (ctx: bool SMap.t) (decl: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.Declarator.t): expr =
     let decl' = snd decl in
     match decl' with {id = id; init = init} ->
     let id' = snd id in 
@@ -49,34 +90,34 @@ let desugar_declarator (ctx: bool SMap.t) (decl: (Loc.t, Loc.t) Flow_ast.Stateme
         | Some false -> raise @@ Failure "Not assignable"
         | None -> (* It's global. if it exists, do nothing, else set to undefined. *)
             let e = desugar_declarator_init ctx init in
-            SetRef (Id "$global", (UpdateField (Deref (Id "$global"), String name'.name, e)))
+            SetRef (Id "$global", (UpdateField (Deref (Id "$global"), EString name'.name, e)))
         )
     | _ -> raise @@ Failure "Only Identifier is supported"
-;;
 
+and
 
-let desugar_variableDeclaration (ctx: bool SMap.t) (decls: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.t): expr =
+desugar_variableDeclaration (ctx: bool SMap.t) (decls: (Loc.t, Loc.t) Flow_ast.Statement.VariableDeclaration.t): expr =
     match decls with {declarations = declarations} ->
     List.fold_right (fun l r -> ESeq (l, r)) (List.map (desugar_declarator ctx) declarations) Undefined
-;;
+
+and 
 
 (* statement is the top level element in js *)
-let desugar_stmt (ctx: bool SMap.t) (stmt: (Loc.t, Loc.t) Flow_ast.Statement.t): expr =
+desugar_stmt (ctx: bool SMap.t) (stmt: (Loc.t, Loc.t) Flow_ast.Statement.t): expr =
     let stmt' = snd stmt in
     match stmt' with
     | VariableDeclaration var ->  desugar_variableDeclaration ctx var
     | Expression expr ->  desugar_expr ctx expr.expression
     | _ -> raise @@ Failure "Only VariableDeclaration is supported"
-;;
 
+and
 
-let desugar ((prog, _): (Loc.t, Loc.t) Flow_ast.Program.t * 'b): expr =
+desugar ((prog, _): (Loc.t, Loc.t) Flow_ast.Program.t * 'b): expr =
     let prog' = snd prog in 
     let stmts = prog'.statements in
     List.fold_right (fun l r -> ESeq (l, r)) (List.map (desugar_stmt SMap.empty) stmts) Undefined
 ;;
 
-let ast: expr = desugar @@ Parser_flow.program "var liwei = 0, ocaml = 6;";;
 
 
 let parens (cmd: string) (content: string): string =
@@ -93,9 +134,12 @@ let rec str (e: expr): string =
     | Undefined -> "undefined"
     | Num n -> " " ^ string_of_float n
     | Id id -> id
-    | String s -> "\"" ^ s ^ "\""
+    | EString s -> "\"" ^ s ^ "\""
     | _ -> raise @@ Failure "Not supported print" 
 ;;
 
 
+let ast: expr = desugar @@ Parser_flow.program "var liwei = 0, ocaml = 6;";;
 let c = print_string @@ "(let (($global (alloc (object))))" ^ (str ast) ^ ")\n";;
+
+let ast: expr = desugar @@ Parser_flow.program "var v = {'a': 6, 'answer': 42}";;
